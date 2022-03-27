100ad7852e6ffa69b269012e62b4f91970ed6dfd
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2022-03-26 20:07:27 -0600

    initial commit

---
+++ natee-cherie/natee-cherie/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  natee-cherie
+//
+//  Created by Dan Orton on 3/26/22.
+//
+
+import SwiftUI
+
+let affirmations = ["Classy",
+                    "Friendly",
+                    "Sunshiny",
+                    "Kind",
+                    "Smart",
+                    "Humble"]
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ natee-cherie/natee-cherie/natee_cherieApp.swift
@@ -0,0 +1,17 @@
+//
+//  natee_cherieApp.swift
+//  natee-cherie
+//
+//  Created by Dan Orton on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct natee_cherieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

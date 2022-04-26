b3f8bf028cdf1cce60ac9fb0ba97b56d23f7e35a
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2022-04-25 21:19:48 -0600

    initial commit

---
+++ Affirmations-iOS/Affirmations-iOS/Affirmations_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Affirmations_iOSApp.swift
+//  Affirmations-iOS
+//
+//  Created by Dan Orton on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Affirmations_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
